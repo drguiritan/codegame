821fdc780333ad8a98ed207a36dfdc38a6cb6bc7
justin120522 <justin.tejano@urios.edu.ph>
2025-03-24 07:16:36 +0000

    code change

--- game.c
+++ game.c
@@ -1,12 +1,3 @@
-#include <iostream>
-
-int main() {
-    // Directly print the name "Justin Tejano"
-    std::cout << "Justin Tejano" << std::endl;
-
-    return 0;
-=======
-
 #include <stdio.h>
 int main() {
 printf("Terrence Rodriguez");
@@ -15,7 +6,7 @@ printf("Hi!");
 printf("genie_castillo")
 printf("boy boy");
 printf("Zachary Alaba");
- printf("Justin Tejano");
+printf("Justin Tejano");
 
 return 0;
 }

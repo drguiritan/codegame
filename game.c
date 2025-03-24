ea6e0a5d508c79231679a7921484989ca176cd1d
ManoyJoseph <jpujida11@gmail.com>
2025-03-24 07:16:09 +0000

    Initial code

--- game.c
+++ game.c
@@ -1,38 +1,5 @@
 
-	
-
-
-
-  
-
 int main() {
-  printf("Alexander Liles");
-
-    printf("Junrich Menardo!/n");
-	printf("Raper");
-  printf("boy boy");
-  printf("Nadz");
-printf("Terrence Rodriguez");
-printf("ed_mendoza")
-printf("Hi!");
-printf("genie_castillo")
-printf("boy boy");
-printf("Zachary Alaba");
-
-
-printf("Carl Platil");
-
-printf("SHADRACH MALAQUE")
-
-printf("Cresan Cuanan");
-printf("Kent Hinayon");
-printf("Julian Tatoy"); 
-printf("Luke Gonzaga"); 
-    return 0;
-
-
-printf("Giducos")
-
-parint("Rosales")
+printf("Jospeh Pujida")
 }
 

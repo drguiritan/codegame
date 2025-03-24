7585aafb316714acfc2114b4b55b5218c4c04b7b
Liles21 <alexander.liles@urios.edu.ph>
2025-03-24 07:07:00 +0000

    Initial game

--- game.c
+++ game.c
@@ -1,15 +1,6 @@
 int main() {
-	printf("Raper");
-
-
-
-printf("Terrence Rodriguez");
-printf("ed_mendoza")
-printf("Hi!");
-printf("genie_castillo")
-printf("boy boy");
-printf("Zachary Alaba");
-printf("Kent Hinayon");
+	
+printf("Alexander Liles");
     return 0;
 }
 

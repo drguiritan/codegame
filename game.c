5f47adeb92bd60b033918af11b0a94b9e62ea420
rrrrynnnns <harry.banas@urios.edu.ph>
2025-03-24 07:33:37 +0000

    code change

--- game.c
+++ game.c
@@ -1,45 +1,6 @@
-
-
-
-
-  
-
 int main() {
-      print("Monalia Almero!/n");
-  printf("Jospeh Pujida")
-  printf("Alexander Liles");
-
-    printf("Junrich Menardo!/n");
-	printf("Raper");
-  printf("boy boy");
-  printf("Nadz");
-printf("Terrence Rodriguez");
-printf("ed_mendoza")
-printf("Hi!");
-printf("genie_castillo")
-printf("boy boy");
-printf("Zachary Alaba");
-
-
-printf("Carl Platil");
-
-printf("SHADRACH MALAQUE")
-
-printf("Cresan Cuanan");
-printf("Kent Hinayon");
-printf("Julian Tatoy"); 
-
-printf("Luke Gonzaga"); 
-
-printf("Luke Gonzaga");
-printf("John galito");
-
-
-
-printf("Goco")
-printf("Giducos")
-
-parint("Rosales")
-
+      
+printf("Harry Banas")
+    return 0;
 }
 
